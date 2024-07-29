4c69bf90850cb61b90c7135cfb3e9e83f27a3089
K1dou <hique1276@gmail.com>
2024-07-28 23:54:59 -0300

    adicionado model para serviço de notificaçao

---
+++ notificacao-service/src/main/java/com/kidou/notificacao_service/model/Pedido.java
@@ -0,0 +1,81 @@
+package com.kidou.notificacao_service.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kidou.notificacao_service.model.enums.Status;
+
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Pedido {
+
+    private UUID id = UUID.randomUUID();
+    private String cliente;
+    private List<ItemPedido> itemPedidos = new ArrayList<>();
+    private Double valorTotal;
+    private String emailNotificacao;
+    private Status status = Status.EM_PROCESSAMENTO;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataHora = LocalDateTime.now();
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Pedido() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemPedido> getItemPedidos() {
+        return itemPedidos;
+    }
+
+    public void setItemPedidos(List<ItemPedido> itemPedidos) {
+        this.itemPedidos = itemPedidos;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getEmailNotificacao() {
+        return emailNotificacao;
+    }
+
+    public void setEmailNotificacao(String emailNotificacao) {
+        this.emailNotificacao = emailNotificacao;
+    }
+}
