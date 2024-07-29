c57bee7a17507afc2eecda7a85a8d9639a8a3ed5
K1dou <hique1276@gmail.com>
2024-07-28 23:23:20 -0300

    first commit

---
+++ notificacao-service/src/test/java/com/kidou/notificacao_service/NotificacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidou.notificacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pedido-api/src/main/java/com/kido/pedido_api/model/Produto.java
@@ -0,0 +1,38 @@
+package com.kido.pedido_api.model;
+
+import java.util.UUID;
+
+public class Produto {
+
+    private UUID id = UUID.randomUUID();
+    private String nome;
+    private Double valor;
+
+
+    public Produto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
