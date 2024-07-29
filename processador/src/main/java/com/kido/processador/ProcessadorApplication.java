8e608d31548495b94b2a93f518d887304d73ae3b
K1dou <hique1276@gmail.com>
2024-07-29 17:29:40 -0300

    29-07-2024 kido

---
+++ processador/src/main/java/com/kido/processador/ProcessadorApplication.java
@@ -0,0 +1,13 @@
+package com.kido.processador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadorApplication.class, args);
+	}
+
+}
---
+++ processador/src/test/java/com/kido/processador/ProcessadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kido.processador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
